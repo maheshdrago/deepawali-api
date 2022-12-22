89b611e6231d95cd315f2cea9911a2db521b2aff
maheshdrago <41584402+maheshdrago@users.noreply.github.com>
2022-12-22 21:26:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from deepawali import app
 from deepawali import views
-from deepawali.views import GetDomainData
+from deepawali.views import GetDomainData, GetKeywordSuggestions, GetKeywordsFromText, GetRelatedKeywords, GetKeywordsFromURL
 from flask_restful import Api
 from flask_cors import CORS
 
@@ -8,6 +8,10 @@ cors = CORS(app)
 api = Api(app)
 
 api.add_resource(GetDomainData, '/domainData')
+api.add_resource(GetKeywordSuggestions, '/keywordSuggestions')
+api.add_resource(GetKeywordsFromText, '/keywordsFromText')
+api.add_resource(GetRelatedKeywords, '/relatedKeywords')
+api.add_resource(GetKeywordsFromURL, '/getKeywordsFromUrl')
 
 if __name__ == '__main__':
     app.run(debug=True)
